2fca549ccc85b7837ef6cfe6c5b07eded0ca81a3
tsmolarczyk <smolarczyk.thomas@gmail.com>
2022-01-31 15:15:11 +0100

    polishing

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('here');
+const enter = document.getElementById("login__enter");
+let input_user = document.getElementById('login__user');
+let input_password = document.getElementById('login__password')
+
+enter.addEventListener('click', function() {
+    let user = input_user.value;
+    let password = input_password.value;
+    console.log(`user: ${user} 
+password: ${password}`);
+    }
+)
+
+
